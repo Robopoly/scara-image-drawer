7a033ad5f1dcbf4eb65300108efe03adad007265
Adrien Beraud <adrien.beraud@epfl.ch>
2011-11-09 15:39:28 +0100

    build system change

---
+++ build/CMakeFiles/edge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/edge.dir/edge.o"
+  "edge.pdb"
+  "edge"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/edge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
